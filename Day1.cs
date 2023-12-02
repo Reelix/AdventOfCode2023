8728b021a87387fba5214a8203e12d46f132dc02
Reelix <reelix+github@gmail.com>
2023-12-02 22:51:56 +0200

    Uploading the solutions to Days 1 and 2

---
+++ Day1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2023
+{
+    internal class Day1
+    {
+        public static void Day1A()
+        {
+            List<string> fileData = File.ReadAllLines(@"C:\AdventOfCode2023\challenge.txt").ToList();
+            int tot = 0;
+            foreach (string line in fileData)
+            {
+                // Get the chars that are digits
+                List<char> chars = line.ToCharArray().Where(x => char.IsDigit(x)).ToList();
+                // Only the first and last
+                string numString = chars[0].ToString() + chars[chars.Count - 1].ToString();
+                int num = int.Parse(numString);
+                tot += num;
+            }
+            Console.WriteLine(tot);
+        }
+
+        public static void Day1B()
+        {
+            List<string> fileData = File.ReadAllLines(@"C:\AdventOfCode2023\challenge.txt").ToList();
+            int tot = 0;
+            foreach (string line in fileData)
+            {
+                // First num
+                int firstNum = -1;
+                for (int j = 0; j <= line.Length; j++)
+                {
+                    firstNum = FindNum(line.Substring(0, j));
+                    if (firstNum != -1)
+                    {
+                        break;
+                    }
+                }
+
+                // Last Num
+                int lastNum = -1;
+                for (int j = 0; j <= line.Length; j++)
+                {
+                    lastNum = FindNum(line.Substring(line.Length - j, j));
+                    if (lastNum != -1)
+                    {
+                        break;
+                    }
+                }
+
+                // Convert them to a string representation
+                string numString = firstNum + "" + lastNum;
+
+                // And get the numerical value of it
+                int num = int.Parse(numString);
+                tot += num;
+            }
+            Console.WriteLine(tot);
+        }
+
+        static int FindNum(string input)
+        {
+            // Check for numerics
+            List<char> chars = input.ToCharArray().Where(x => char.IsDigit(x)).ToList();
+            if (chars.Any())
+            {
+                char firstChar = chars[0];
+                return int.Parse(firstChar.ToString());
+            }
+
+            // Check for word version
+            // This technically doesn't work given the example as I hadn't coded in the xteen's, but my final data set didn't have any
+            Dictionary<string, int> wordNumbers = new Dictionary<string, int>()
+            {
+                {"one", 1},
+                {"two", 2},
+                {"three", 3},
+                {"four", 4},
+                {"five", 5},
+                {"six", 6},
+                {"seven", 7},
+                {"eight", 8},
+                {"nine", 9}
+            };
+
+            foreach (var item in wordNumbers)
+            {
+                if (input.Contains(item.Key))
+                {
+                    return item.Value;
+                }
+            }
+
+            // Nothing - Continue loop
+            return -1;
+        }
+    }
+
+}
---
+++ Day2.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2023
+{
+    internal class Day2
+    {
+        public static void Day2A()
+        {
+            List<string> fileData = File.ReadAllLines(@"C:\AdventOfCode2023\input.txt").ToList();
+            int totIds = 0;
+            int gameId = 1;
+
+            // Iterate through all games
+            foreach (string line in fileData)
+            {
+                bool isValid = true;
+
+                // Remove the game text
+                string cubes = line.Remove(0, line.IndexOf(":") + 2);
+
+                // Iterate through each set
+                List<string> setList = cubes.Split(';').ToList();
+                foreach (string set in setList)
+                {
+                    int red = 0;
+                    int green = 0;
+                    int blue = 0;
+                    List<string> cubeSet = set.Split(' ').ToList();
+                    cubeSet = cubeSet.Where(x => x != "").ToList();
+
+                    // Count the number of cubes per set
+                    for (int j = 0; j < cubeSet.Count; j += 2)
+                    {
+                        int count = int.Parse(cubeSet[j]);
+                        string cubeColor = cubeSet[j + 1];
+                        if (cubeColor.Contains("red"))
+                        {
+                            red += count;
+                        }
+                        else if (cubeColor.Contains("green"))
+                        {
+                            green += count;
+                        }
+                        else if (cubeColor.Contains("blue"))
+                        {
+                            blue += count;
+                        }
+                    }
+
+                    // If a set contains too many of a single cube type, it's invalid
+                    if (red > 12 || green > 13 || blue > 14)
+                    {
+                        isValid = false;
+                    }
+                }
+
+                // If the set is valid, add it to the total
+                if (isValid)
+                {
+                    totIds += gameId;
+                }
+
+                // The games are sequential, so no need to calculate the ID each time
+                gameId++; 
+            }
+            Console.WriteLine(totIds);
+        }
+
+        public static void Day2B()
+        {
+            List<string> fileData = File.ReadAllLines(@"C:\AdventOfCode2023\input.txt").ToList();
+            int totPower = 0;
+
+            // Iterate through all games
+            foreach (string line in fileData)
+            {
+                int minRed = 0;
+                int minGreen = 0;
+                int minBlue = 0;
+                bool isValid = true;
+
+                // Remove the game text
+                string cubes = line.Remove(0, line.IndexOf(":") + 2);
+
+                // Iterate through each set
+                List<string> setList = cubes.Split(';').ToList();
+                foreach (string set in setList)
+                {
+                    List<string> cubeSet = set.Split(' ').ToList();
+                    cubeSet = cubeSet.Where(x => x != "").ToList();
+
+                    // Count the number of cubes per set
+                    for (int j = 0; j < cubeSet.Count; j += 2)
+                    {
+                        int count = int.Parse(cubeSet[j]);
+                        string cubeColor = cubeSet[j + 1];
+
+                        // If the amount of the color is larger than the max count for the color in the game, increase it
+                        if (cubeColor.Contains("red"))
+                        {
+                            if (count > minRed)
+                            {
+                                minRed = count;
+                            }
+                        }
+                        else if (cubeColor.Contains("green"))
+                        {
+                            if (count > minGreen)
+                            {
+                                minGreen = count;
+                            }
+                        }
+                        else if (cubeColor.Contains("blue"))
+                        {
+                            if (count > minBlue)
+                            {
+                                minBlue = count;
+                            }
+                        }
+                    }
+                }
+
+                int power = minRed * minBlue * minGreen;
+                totPower += power;
+            }
+            Console.WriteLine(totPower);
+        }
+    }
+}
